977d050328e823b17ec1b0ed059d5bfdb06090e3
Mark Shvabenlandt <rasslabone@gmail.com>
2024-09-21 16:09:49 +0300

    fix schema url applying during build 2

--- lib/src/schema_downloader.dart
+++ lib/src/schema_downloader.dart
@@ -1,16 +1,21 @@
-import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:io';
 
 class SchemaDownloader {
   static Future<String> downloadSchema(String url) async {
-    final response = await http.post(
-      Uri.parse(url),
-      headers: {'Content-Type': 'application/json'},
-      body:
-          '{"query": "{ __schema { types { name kind description fields { name type { name kind ofType { name kind } } } } } }"}',
-    );
+    HttpClient client = HttpClient()
+      ..badCertificateCallback =
+          ((X509Certificate cert, String host, int port) => true);
+
+    final request = await client.postUrl(Uri.parse(url));
+    request.headers.set('Content-Type', 'application/json');
+    request.write(
+        '{"query": "{ __schema { types { name kind description fields { name type { name kind ofType { name kind } } } } } }"}');
+
+    final response = await request.close();
 
     if (response.statusCode == 200) {
-      return response.body;
+      return await response.transform(utf8.decoder).join();
     } else {
       throw Exception('Failed to download schema: ${response.statusCode}');
     }
