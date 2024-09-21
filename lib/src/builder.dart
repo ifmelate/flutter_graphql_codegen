8bac84012c7e819a299d887d93734ef2c7629ac3
Mark Shvabenlandt <rasslabone@gmail.com>
2024-09-21 13:15:15 +0300

    try another builder approach

--- lib/src/builder.dart
+++ lib/src/builder.dart
@@ -1,9 +1,5 @@
 import 'package:build/build.dart';
-import 'package:glob/glob.dart';
-import 'package:path/path.dart' as path;
 import 'config.dart';
-import 'generator.dart';
-import 'schema_downloader.dart';
 
 class GraphQLCodegenBuilder implements Builder {
   final GraphQLCodegenConfig config;
@@ -12,27 +8,19 @@ class GraphQLCodegenBuilder implements Builder {
 
   @override
   Future<void> build(BuildStep buildStep) async {
-    final schema = await SchemaDownloader.downloadSchema(config.schemaUrl);
-    final graphqlFiles = buildStep.findAssets(Glob('**/*.graphql'));
-
-    final documents = await Future.wait(graphqlFiles.map((asset) async {
-      final content = await buildStep.readAsString(asset);
-      return content;
-    }));
-
-    final generatedCode =
-        GraphQLCodeGenerator.generateClientCode(schema, documents);
-
-    final outputId = AssetId(
-      buildStep.inputId.package,
-      path.join(config.outputDir, 'generated_client.dart'),
-    );
-
-    await buildStep.writeAsString(outputId, generatedCode);
+    // ... (оставьте остальной код без изменений)
   }
 
   @override
   Map<String, List<String>> get buildExtensions => {
         r'$lib$': ['generated_client.dart'],
       };
+}
+
+Builder graphqlCodegenBuilder(BuilderOptions options) {
+  final yamlString = options.config['graphql_codegen'] as String?;
+  if (yamlString == null) {
+    throw ArgumentError('Missing configuration for GraphQLCodegenBuilder');
+  }
+  return GraphQLCodegenBuilder(GraphQLCodegenConfig.fromYaml(yamlString));
 }
