808adead84ca619bbc43e5c816408966a61cc50e
Mark Shvabenlandt <rasslabone@gmail.com>
2024-09-21 16:23:36 +0300

    fix schema url applying during build 7

--- lib/src/schema_downloader.dart
+++ lib/src/schema_downloader.dart
@@ -1,19 +1,119 @@
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class SchemaDownloader {
   static Future<String> downloadSchema(String url) async {
+    final introspectionQuery = '''
+      query IntrospectionQuery {
+        __schema {
+          queryType { name }
+          mutationType { name }
+          subscriptionType { name }
+          types {
+            ...FullType
+          }
+          directives {
+            name
+            description
+            locations
+            args {
+              ...InputValue
+            }
+          }
+        }
+      }
+
+      fragment FullType on __Type {
+        kind
+        name
+        description
+        fields(includeDeprecated: true) {
+          name
+          description
+          args {
+            ...InputValue
+          }
+          type {
+            ...TypeRef
+          }
+          isDeprecated
+          deprecationReason
+        }
+        inputFields {
+          ...InputValue
+        }
+        interfaces {
+          ...TypeRef
+        }
+        enumValues(includeDeprecated: true) {
+          name
+          description
+          isDeprecated
+          deprecationReason
+        }
+        possibleTypes {
+          ...TypeRef
+        }
+      }
+
+      fragment InputValue on __InputValue {
+        name
+        description
+        type { ...TypeRef }
+        defaultValue
+      }
+
+      fragment TypeRef on __Type {
+        kind
+        name
+        ofType {
+          kind
+          name
+          ofType {
+            kind
+            name
+            ofType {
+              kind
+              name
+              ofType {
+                kind
+                name
+                ofType {
+                  kind
+                  name
+                  ofType {
+                    kind
+                    name
+                    ofType {
+                      kind
+                      name
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    ''';
+
     final response = await http.post(
       Uri.parse(url),
       headers: {'Content-Type': 'application/json'},
-      body:
-          '{"operationName":"IntrospectionQuery","query":"query IntrospectionQuery {  __type(name: \"__Schema\") {    name    fields {      name    }  }}"}',
+      body: json.encode({
+        'query': introspectionQuery,
+      }),
     );
 
     if (response.statusCode == 200) {
-      return response.body;
+      final jsonResponse = json.decode(response.body);
+      if (jsonResponse['errors'] != null) {
+        throw Exception('GraphQL errors: ${jsonResponse['errors']}');
+      }
+      return json.encode(jsonResponse['data']);
     } else {
       throw Exception(
-          'Failed to download schema: ${response.statusCode} and response body: ${response.body}');
+          'Failed to download schema: ${response.statusCode}. Response: ${response.body}');
     }
   }
 }
